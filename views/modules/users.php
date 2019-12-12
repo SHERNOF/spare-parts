0b4b9c3e8618e7ccf5ef2a12b05f2dfbe2055da1
Shernof <sherwin.nofuente@yahoo.com>
2019-12-12 17:26:43 +0800

    3

--- views/modules/users.php
+++ views/modules/users.php
@@ -62,7 +62,7 @@
                         <td>admin</td>
                         <td><img src="views/img/users/default/anonymous.png" class="img-thumbnail" width="40px"></td>
                         <td>Administrator</td>
-                        <td><button class="btn btn-success btn-xs">Activate</button></td>
+                        <td><button class="btn btn-success btn-xs">Activated</button></td>
                         <td>2019-12-25 12:00:00</td>
                         <td>
                           <div class="btn-group"> 
@@ -70,9 +70,9 @@
                                 <button class="btn btn-danger"><i class="fa fa-times"></i></button>
                           </div>
                         </td>
-                       </tr> 
+                     </tr> 
 
-                        <tr>  
+                      <tr>  
                         <td>1</td>
                         <td>User Administrator</td>
                         <td>admin</td>
@@ -86,7 +86,24 @@
                                 <button class="btn btn-danger"><i class="fa fa-times"></i></button>
                           </div>
                         </td>
-                       </tr> 
+                     </tr> 
+
+
+                     <tr>  
+                        <td>1</td>
+                        <td>User Administrator</td>
+                        <td>admin</td>
+                        <td><img src="views/img/users/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+                        <td>Administrator</td>
+                        <td><button class="btn btn-success btn-xs">Activated</button></td>
+                        <td>2019-12-25 12:00:00</td>
+                        <td>
+                          <div class="btn-group"> 
+                                <button class="btn btn-wwarning"><i class="fa fa-pencil"></i></button>
+                                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                          </div>
+                        </td>
+                     </tr> 
 
                   </tbody>
 
@@ -105,21 +122,77 @@
   <div class="modal-dialog">
     <div class="modal-content">
 
+      <form role="form" method="post" enctype="multipart/form-data">
+
       <!-- Modal Header -->
-      <div class="modal-header">
+      <div class="modal-header" style="background: #3c8dbc; color: white">
         <h4 class="modal-title">Nice Sherwin</h4>
         <button type="button" class="close" data-dismiss="modal">&times;</button>
       </div>
 
       <!-- Modal body -->
       <div class="modal-body">
-        Modal body..
+        <div class="box-body">
+
+          <!-- for entry fir number -->
+          <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                <input type="text" class="form-control input-lg" name="newNumber" placeholder="Insert Number" required="">
+              </div>
+          </div>    
+
+              <!-- user entry -->
+          <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-key"></i></span>
+                <input type="text" class="form-control input-lg" name="newUser" placeholder="Add User" required="">
+              </div>
+          </div>
+
+          <!-- password entry -->
+          <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                <input type="text" class="form-control input-lg" name="newPassword" placeholder="Input password" required="">
+              </div>
+          </div>
+
+            <!-- user profile entry -->
+          <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-users"></i></span>
+                <select class="form-control input-lg" name="newProfile">
+                
+                  <option value="">Select Profile</option>
+                  <option value="Administrator">Administrator</option>
+                  <option value="Special">Special</option>
+                  <option value="Seller">Seller</option>
+
+                </select>
+              </div>
+          </div>
+
+            <!-- pictures entry -->
+          <div class="form-group">
+            <div class="panel">Upload Profile Photo</div>
+            <input type="file" id="newPhoto" name="newPhoto">
+            <p class="help-block">Only max of 200MB per Photo</p>
+            <img src="views/img/users/default/anonymous.png" class="img-thumbnail" width="100px">
+          </div>          
+
+
+  
+        </div>
       </div>
 
       <!-- Modal footer -->
       <div class="modal-footer">
-        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-default oull-left" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Save Changes</button>
       </div>
+
+    </form>
 
     </div>
   </div>
