9f931f6ab4cfc508e93f7c92f990d55ff62c3438
Shernof <sherwin.nofuente@yahoo.com>
2019-12-24 23:42:33 +0800

    1

--- models/connection.php
+++ models/connection.php
@@ -4,7 +4,7 @@
 
 class Connection {
 	static public function connect(){
-		$link = new PDO("mysql:host=localhost;dbname=spr", "root", "");
+		$link = new PDO("mysql:host=localhost;dbname=spare", "root", "");
 
 		$link -> exec("set names utf8");
 
