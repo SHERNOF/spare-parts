f5e8dd913d0bf558cec2fd28e0a168c99ba5b425
Sherwin Nofuente <sherwinnofuente@Sherwins-MacBook-Pro.local>
2020-01-01 22:50:23 +0800

    1

--- views/modules/parts.php
+++ views/modules/parts.php
@@ -27,7 +27,7 @@
       <div class="box">
         <div class="box-header with-border">
 
-            <button class="btn btn-primary" data-toggle="modal" data-target="#addUser" >
+            <button class="btn btn-primary" data-toggle="modal" data-target="#addPart" >
               Add Part
             </button>
       
@@ -35,7 +35,7 @@
 
         <div class="box-body">
 
-            <table class="table table-bordered table-striped dt-responsive tables"> 
+            <table class="table table-bordered table-striped dt-responsive tables" width="100%"> 
 
                 <thead> 
 
@@ -61,27 +61,13 @@
 
                       <tr>  
                         <td>1</td>
-                        <td><img src="views/img/parts/default/anonymous.jpg" class=""></td>
-                        <td>admin</td>
-                        <td><img src="views/img/users/default/anonymous.png" class="img-thumbnail" width="40px"></td>
-                        <td>Administrator</td>
-                        <td><button class="btn btn-success btn-xs">Activated</button></td>
-                        <td>2019-12-25 12:00:00</td>
-                        <td>
-                          <div class="btn-group"> 
-                                <button class="btn btn-wwarning"><i class="fa fa-pencil"></i></button>
-                                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
-                          </div>
-                        </td>
-                     </tr> 
-
-                      <tr>  
+                        <td><img src="views/img/parts/anonymous.png" class="img-thumbnail" width="40px"></td>
+                        <td>0001</td>
+                        <td>Helical</td>
+                        <td>AT70</td>
                         <td>1</td>
-                        <td>User Administrator</td>
-                        <td>admin</td>
-                        <td><img src="views/img/users/default/anonymous.png" class="img-thumbnail" width="40px"></td>
-                        <td>Administrator</td>
-                        <td><button class="btn btn-danger btn-xs">Deactivated</button></td>
+                        <td>40</td>
+                        <td>60</td>
                         <td>2019-12-25 12:00:00</td>
                         <td>
                           <div class="btn-group"> 
@@ -90,15 +76,16 @@
                           </div>
                         </td>
                      </tr> 
-
 
                      <tr>  
                         <td>1</td>
-                        <td>User Administrator</td>
-                        <td>admin</td>
-                        <td><img src="views/img/users/default/anonymous.png" class="img-thumbnail" width="40px"></td>
-                        <td>Administrator</td>
-                        <td><button class="btn btn-success btn-xs">Activated</button></td>
+                        <td><img src="views/img/parts/anonymous.png" class="img-thumbnail" width="40px"></td>
+                        <td>0001</td>
+                        <td>Helical</td>
+                        <td>AT70</td>
+                        <td>1</td>
+                        <td>40</td>
+                        <td>60</td>
                         <td>2019-12-25 12:00:00</td>
                         <td>
                           <div class="btn-group"> 
@@ -108,20 +95,39 @@
                         </td>
                      </tr> 
 
-                  </tbody>
+                     <tr>  
+                        <td>1</td>
+                        <td><img src="views/img/parts/anonymous.png" class="img-thumbnail" width="40px"></td>
+                        <td>0001</td>
+                        <td>Helical</td>
+                        <td>AT70</td>
+                        <td>1</td>
+                        <td>40</td>
+                        <td>60</td>
+                        <td>2019-12-25 12:00:00</td>
+                        <td>
+                          <div class="btn-group"> 
+                                <button class="btn btn-wwarning"><i class="fa fa-pencil"></i></button>
+                                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                          </div>
+                        </td>
+                     </tr> 
 
+                      
+
+                  </tbody>
             </table>
-          <!-- Start creating your amazing application! -->
         </div>
       </div>
     </section>
   </div>
 
 
-<!-- Modal -->
+    <!--=====================================
+    =     Module Add Parts            =
+    ======================================-->
 
-<!-- The Modal -->
-<div class="modal fade" id="addUser" role="dialog">
+<div class="modal fade" id="addPart" role="dialog">
 
   <div class="modal-dialog">
 
@@ -134,78 +140,114 @@
       <div class="modal-header" style="background: #3c8dbc; color: #fff">
         
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Add User</h4>
+        <h4 class="modal-title">Add Product</h4>
       </div>
 
       <!-- Modal body -->
       <div class="modal-body">
         <div class="box-body">
 
-          <!-- for entry fir number -->
+          <!-- for Id number -->
+
           <div class="form-group">
               <div class="input-group">
-                <span class="input-group-addon"><i class="fa fa-user"></i></span>
-                <input type="text" class="form-control input-lg" name="newName" placeholder="Add Name" required>
+                <span class="input-group-addon"><i class="fa fa-code"></i></span>
+                <input type="text" class="form-control input-lg" name="newId" placeholder="Add Id Number" required>
               </div>
           </div>    
 
-              <!-- user entry -->
+              <!-- Description Entry -->
           <div class="form-group">
               <div class="input-group">
-                <span class="input-group-addon"><i class="fa fa-key"></i></span>
-                <input type="text" class="form-control input-lg" name="newUser" placeholder="Add username" required>
+                <span class="input-group-addon"><i class="fa fa-product-hunt"></i></span>
+                <input type="text" class="form-control input-lg" name="newDescription" placeholder="Add Description" required>
               </div>
           </div>
 
-          <!-- password entry -->
-          <div class="form-group">
-              <div class="input-group">
-                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
-                <input type="password" class="form-control input-lg" name="newPasswd" placeholder="Input password" required>
-              </div>
-          </div>
+            <!-- Categories -->
 
-            <!-- user profile entry -->
           <div class="form-group">
               <div class="input-group">
-                <span class="input-group-addon"><i class="fa fa-users"></i></span>
+                <span class="input-group-addon"><i class="fa fa-th"></i></span>
                 <select class="form-control input-lg" name="newProfile">
                 
-                  <option value="">Select Profile</option>
-                  <option value="Administrator">Administrator</option>
-                  <option value="Special">Special</option>
-                  <option value="Seller">Seller</option>
+                  <option value="">Select Category</option>
+                  <option value="AT70">AT70</option>
+                  <option value="Merlin">Merlin</option>
+                  <option value="Wren">Wren</option>
 
                 </select>
               </div>
           </div>
 
-            <!-- pictures entry -->
-          <div class="form-group">
-            <div class="panel">Upload Profile Photo</div>
-            <input type="file" id="newPhoto" name="newPhoto">
-            <p class="help-block">Only max of 200MB per Photo</p>
-            <img src="views/img/users/default/anonymous.png" class="img-thumbnail" width="100px">
-          </div>          
+              <!-- Stock Entry -->
+              <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-check"></i></span>
+                <input type="number" class="form-control input-lg" name="newStock" min="0" placeholder="Stock" required>
+              </div>
+          </div>
 
+              <!-- Buying Prince Entry -->
+              <div class="form-group row">
 
-  
-        </div>
-      </div>
+              <div class="col-xs-6">
+                    <div class="input-group">
+                      <span class="input-group-addon"><i class="fa fa-arrow-up"></i></span>
+                      <input type="number" class="form-control input-lg" name="newPriceBuy" min="0" placeholder="Buying Price" required>
+                    </div>
+                  </div>
+                
+              <!-- Selling Price Entry -->
+              <div class="col-xs-6">
+                    <div class="input-group">
+                      <span class="input-group-addon"><i class="fa fa-arrow-down"></i></span>
+                      <input type="number" class="form-control input-lg" name="newPriceSell" min="0" placeholder="Selling Price" required>
+                  </div>
 
-      <!-- Modal footer -->
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default oull-left" data-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">Save Changes</button>
-      </div>
+                  <br>
 
-     <?php
-      $createUser = new ControllerUsers();
-      $createUser -> ctrCreateUser();
-      ?> 
+              <!-- Percentage Checkbox -->
 
-    </form>
+              <div class="col-xs-6">
+                <div class="form-group">
+                  <label>
+                    <input type="checkbox" class="minimal percentage" checked>
+                    Use PerCentage
+                  </label>
+                </div>
+              </div>
 
+              <div class="col-xs-6" style="padding:0">
+                <div class="input-group"> 
+                  <input type="number" class="form-control input-lg newPercentage" min="0" value="40" required>
+                  <span class="input-group-addon"><i class="fa fa-percent"></i></span>
+              </div>                  
+            </div>
+          </div>
+
+            <!-- Picture entry -->
+              <div class="form-group">
+                <div class="panel">Upload Profile Photo</div>
+                <input type="file" id="newPhoto" name="newPhoto">
+                <p class="help-block">Only max of 2MB per Photo</p>
+                <img src="views/img/parts/anonymous.png" class="img-thumbnail" width="100px">
+              </div>          
+            </div>
+          </div>
+
+          <!-- Modal footer -->
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default oull-left" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Save Part</button>
+          </div>
+
+          <?php
+              // $createPart = new ControllerParts();
+              // $createPart -> ctrCreatePart();
+          ?> 
+          
+      </form>
     </div>
   </div>
 </div>
--- views/template.php
+++ views/template.php
@@ -40,6 +40,10 @@
 
   <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic"> 
+
+     <!-- iCheck for checkboxes and radio inputs -->
+     <link rel="stylesheet" href="views/plugins/iCheck/all.css">
+  
   
   <!--====  End of Plugins CSS  ====-->
   
@@ -69,6 +73,11 @@
 
   <!-- By default sweetalert2 doesn't support IE. To enable IE 11 support, include Promise polyfill -->
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
+
+
+
+   <!-- iCheck 1.0.1 -->
+<script src="views/plugins/iCheck/icheck.min.js"></script>
   
 </head>
 
