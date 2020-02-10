2430acb91d95967d597abd3c45c02d9929598e3a
shernof <sherwin.nofuente@yahoo.com>
2020-02-10 21:44:15 +0800

    1

--- views/js/withdrawal.js
+++ views/js/withdrawal.js
@@ -707,10 +707,10 @@ $(".formWithdrawal").on("change", "input#newCashValue", function(){
 	
 	var cash = $(this).val();
 
-  var totalPrice = $("#newPartsTotalSell").attr("totalSale"); //sherwin
+  // var totalPrice = $("#newPartsTotalSell").attr("totalSale"); //sherwin
 
-	// var change =  Number(cash) - Number($('#saleTotal').val());
-  var change =  Number(cash) - Number($(totalPrice).val());
+	var change =  Number(cash) - Number($('#saleTotal').val());
+  // var change =  Number(cash) - Number($(totalPrice).val());
 
 	var newCashChange = $(this).parent().parent().parent().children('#getCashChange').children().children('#newCashChange');
 
